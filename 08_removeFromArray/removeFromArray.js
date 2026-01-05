de54a346adae0be5e0ea0b2a217855dca5d69105
Minnie The Dev <minniethedev@gmail.com>
2026-01-05 16:14:56 +1100

    Complete exercises 6-8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    for (const arg of args) {
+
+        let argInArr = (arr.filter(item => item === arg)).length
+        while (argInArr) {
+            arr.splice(arr.findIndex(item => item === arg), 1)
+            argInArr--
+        }
+    }
+
+    return arr
 };
 
 // Do not edit below this line
